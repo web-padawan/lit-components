11fd8c05ff9a52ab6f26f798e20cf4aabbc3029e
web-padawan <serguey.kulikov@gmail.com>
2018-09-14 15:54:12 +0300

    Add item-mixin

---
+++ packages/item-mixin/lit-item-mixin.js
@@ -0,0 +1,145 @@
+/**
+ * @polymerMixin
+ */
+export const ItemMixin = superClass =>
+  class extends superClass {
+    /**
+     * Used for mixin detection because `instanceof` does not work with mixins.
+     */
+    static get hasLitItemMixin() {
+      return true;
+    }
+
+    static get properties() {
+      return {
+        /**
+         * If true, the user cannot interact with this element.
+         */
+        disabled: {
+          type: Boolean,
+          reflect: true
+        },
+
+        /**
+         * If true, the item is in selected state.
+         */
+        selected: {
+          type: Boolean,
+          reflect: true
+        }
+      };
+    }
+
+    constructor() {
+      super();
+
+      if (!this.hasAttribute('selected')) {
+        this.selected = false;
+      }
+    }
+
+    get value() {
+      return this._value !== undefined ? this._value : this.textContent.trim();
+    }
+
+    set value(value) {
+      this._value = value;
+    }
+
+    update(props) {
+      super.update(props);
+
+      if (props.has('disabled')) {
+        this._disabledChanged(this.disabled);
+      }
+      if (props.has('selected')) {
+        this._selectedChanged(this.selected);
+      }
+    }
+
+    firstUpdated() {
+      super.firstUpdated();
+
+      const attrValue = this.getAttribute('value');
+      if (attrValue !== null) {
+        this.value = attrValue;
+      }
+
+      this.addEventListener('focus', e => this._setFocused(true), true);
+      this.addEventListener('blur', e => this._setFocused(false), true);
+      this.addEventListener('mousedown', e => {
+        this._setActive((this._mousedown = true));
+        const mouseUpListener = () => {
+          this._setActive((this._mousedown = false));
+          document.removeEventListener('mouseup', mouseUpListener);
+        };
+        document.addEventListener('mouseup', mouseUpListener);
+      });
+      this.addEventListener('keydown', e => this._onKeydown(e));
+      this.addEventListener('keyup', e => this._onKeyup(e));
+    }
+
+    /**
+     * @protected
+     */
+    disconnectedCallback() {
+      if (super.disconnectedCallback) {
+        super.disconnectedCallback();
+      }
+
+      // in Firefox and Safari, blur does not fire on the element when it is removed,
+      // especially between keydown and keyup events, being active at the same time.
+      if (this.hasAttribute('active')) {
+        this._setFocused(false);
+      }
+    }
+
+    _selectedChanged(selected) {
+      this.setAttribute('aria-selected', selected);
+    }
+
+    _disabledChanged(disabled) {
+      if (disabled) {
+        this.selected = false;
+        this.setAttribute('aria-disabled', 'true');
+        this.blur();
+      } else {
+        this.removeAttribute('aria-disabled');
+      }
+    }
+
+    _setFocused(focused) {
+      if (focused) {
+        this.setAttribute('focused', '');
+        if (!this._mousedown) {
+          this.setAttribute('focus-ring', '');
+        }
+      } else {
+        this.removeAttribute('focused');
+        this.removeAttribute('focus-ring');
+        this._setActive(false);
+      }
+    }
+
+    _setActive(active) {
+      if (active) {
+        this.setAttribute('active', '');
+      } else {
+        this.removeAttribute('active');
+      }
+    }
+
+    _onKeydown(event) {
+      if (/^( |SpaceBar|Enter)$/.test(event.key) && !event.defaultPrevented) {
+        event.preventDefault();
+        this._setActive(true);
+      }
+    }
+
+    _onKeyup() {
+      if (this.hasAttribute('active')) {
+        this._setActive(false);
+        this.click();
+      }
+    }
+  };
---
+++ test/unit/item-mixin.spec.js
@@ -0,0 +1,239 @@
+import { LitElement, html } from '@polymer/lit-element';
+import { render } from 'lit-html';
+import '@polymer/iron-test-helpers/mock-interactions.js';
+import { ItemMixin } from '@lit/item-mixin';
+
+describe('item-mixin', () => {
+  customElements.define(
+    'x-item',
+    class extends ItemMixin(LitElement) {
+      render() {
+        return html`
+          <div>
+            <slot></slot>
+          </div>
+        `;
+      }
+    }
+  );
+
+  function spaceDown(target) {
+    MockInteractions.keyDownOn(target, 32, [], ' ');
+  }
+
+  function spaceUp(target) {
+    MockInteractions.keyUpOn(target, 32, [], ' ');
+  }
+
+  function space(target) {
+    spaceDown(target);
+    spaceUp(target);
+  }
+
+  function enter(target) {
+    MockInteractions.keyDownOn(target, 13, [], 'Enter');
+    MockInteractions.keyUpOn(target, 13, [], 'Enter');
+  }
+
+  function fire(target, type) {
+    target.dispatchEvent(new CustomEvent(type, { composed: true, bubbles: true }));
+  }
+
+  let item;
+
+  describe('with value', () => {
+    const fixture = html`
+      <x-item value="foo">text-content</x-item>
+    `;
+
+    beforeEach(async () => {
+      const div = document.createElement('div');
+      render(fixture, div);
+      item = div.firstElementChild;
+      document.body.appendChild(item);
+      await item.updateComplete;
+    });
+
+    afterEach(() => {
+      if (item.parentNode) {
+        item.parentNode.removeChild(item);
+      }
+    });
+
+    it('should have value property', () => {
+      expect(item.value).to.be.equal('foo');
+    });
+
+    it('should not have focused attribute when not focused', () => {
+      expect(item.hasAttribute('focused')).to.be.false;
+    });
+
+    it('should have focused attribute when focused', () => {
+      fire(item, 'focus');
+      expect(item.hasAttribute('focused')).to.be.true;
+    });
+
+    it('should not have focused attribute when blurred', () => {
+      fire(item, 'focus');
+      fire(item, 'blur');
+      expect(item.hasAttribute('focused')).to.be.false;
+    });
+
+    it('should have active attribute on mousedown', () => {
+      fire(item, 'mousedown');
+      expect(item.hasAttribute('active')).to.be.true;
+      expect(item._mousedown).to.be.true;
+    });
+
+    it('should not have active attribute after mouseup', () => {
+      fire(item, 'mousedown');
+      fire(item, 'mouseup');
+      expect(item.hasAttribute('active')).to.be.false;
+      expect(item._mousedown).to.be.false;
+    });
+
+    it('should have active attribute on space down', () => {
+      spaceDown(item);
+      expect(item.hasAttribute('active')).to.be.true;
+    });
+
+    it('should not have active attribute after space up', () => {
+      spaceDown(item);
+      spaceUp(item);
+      expect(item.hasAttribute('active')).to.be.false;
+    });
+
+    it('should not have active attribute when disconnected from the DOM', () => {
+      spaceDown(item);
+      item.parentNode.removeChild(item);
+      window.ShadyDOM && window.ShadyDOM.flush();
+      expect(item.hasAttribute('active')).to.be.false;
+    });
+
+    it('should not have focus-ring if not focused', () => {
+      expect(item.hasAttribute('focus-ring')).to.be.false;
+    });
+
+    it('should not have focus-ring attribute when not focused with keyboard', () => {
+      item.click();
+      expect(item.hasAttribute('focus-ring')).to.be.false;
+    });
+
+    it('should have focus-ring attribute when focused with keyboard', () => {
+      fire(item, 'focus');
+      expect(item.hasAttribute('focus-ring')).to.be.true;
+    });
+
+    it('should not have focus-ring after blur', () => {
+      fire(item, 'focus');
+      fire(item, 'blur');
+      expect(item.hasAttribute('focus-ring')).to.be.false;
+    });
+
+    it('set this._mousedown to false if mouseup was outside', () => {
+      fire(item, 'mousedown');
+      expect(item._mousedown).to.be.true;
+      fire(document, 'mouseup');
+      expect(item._mousedown).to.be.false;
+    });
+
+    it('should be not selected if disabled', () => {
+      space(item);
+      item.disabled = true;
+      expect(item.selected).to.be.false;
+    });
+
+    it('should not be selectable if disabled', async () => {
+      item.disabled = true;
+      await item.updateComplete;
+      space(item);
+      expect(item.selected).to.be.false;
+    });
+
+    it('should have aria-disabled when disabled', async () => {
+      item.disabled = true;
+      await item.updateComplete;
+      expect(item.getAttribute('aria-disabled')).to.equal('true');
+
+      item.disabled = false;
+      await item.updateComplete;
+      expect(item.getAttribute('aria-disabled')).to.be.null;
+    });
+
+    it('should unselect the current item', () => {
+      item.click();
+      item.click();
+      expect(item.selected).to.be.false;
+    });
+
+    it('should fire click event when it is selected with keyboard', () => {
+      const clickSpy = sinon.spy();
+      item.addEventListener('click', clickSpy);
+      enter(item);
+      expect(clickSpy).to.be.calledOnce;
+    });
+
+    it('should not fire click event if keyup does not happen after a keydown in the element', () => {
+      const clickSpy = sinon.spy();
+      item.addEventListener('click', clickSpy);
+      spaceUp(item);
+      expect(clickSpy).not.to.be.called;
+    });
+  });
+
+  describe('without value', () => {
+    const fixture = html`
+      <x-item>text-content</x-item>
+    `;
+
+    beforeEach(async () => {
+      const div = document.createElement('div');
+      render(fixture, div);
+      item = div.firstElementChild;
+      document.body.appendChild(item);
+      await item.updateComplete;
+    });
+
+    afterEach(() => {
+      item.parentNode.removeChild(item);
+    });
+
+    it('should use trimmed textContent', () => {
+      expect(item.value).to.equal('text-content');
+    });
+
+    it('should reflect changes of content', () => {
+      item.innerHTML = 'foo';
+      expect(item.value).to.equal('foo');
+    });
+  });
+
+  describe('with clickable child', () => {
+    const fixture = html`
+      <x-item>
+        <button>text-content</button>
+      </x-item>
+    `;
+
+    beforeEach(async () => {
+      const div = document.createElement('div');
+      render(fixture, div);
+      item = div.firstElementChild;
+      document.body.appendChild(item);
+      await item.updateComplete;
+    });
+
+    afterEach(() => {
+      item.parentNode.removeChild(item);
+    });
+
+    it('should not set active attribute if keydown was prevented', () => {
+      const button = item.querySelector('button');
+      button.addEventListener('keydown', e => {
+        e.preventDefault();
+      });
+      spaceDown(button);
+      expect(item.hasAttribute('active')).to.be.false;
+    });
+  });
+});
