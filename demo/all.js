f2db5e8af57e343ef9eed2d91b9851acbc7411a8
web-padawan <serguey.kulikov@gmail.com>
2018-12-24 17:07:16 +0200

    chore(demo): update imports in demo to prevent eslint errors

--- demo/all.js
+++ demo/all.js
@@ -1,32 +1,32 @@
-import '@lit/button-lumo/lit-button-lumo.js';
-import '@lit/button-material/lit-button-material.js';
+import '../packages/button-lumo/lit-button-lumo.js';
+import '../packages/button-material/lit-button-material.js';
 
-import '@lit/checkbox-lumo/lit-checkbox-lumo.js';
-import '@lit/checkbox-material/lit-checkbox-material.js';
+import '../packages/checkbox-lumo/lit-checkbox-lumo.js';
+import '../packages/checkbox-material/lit-checkbox-material.js';
 
-import '@lit/checkbox-group-lumo/lit-checkbox-group-lumo.js';
-import '@lit/checkbox-group-material/lit-checkbox-group-material.js';
+import '../packages/checkbox-group-lumo/lit-checkbox-group-lumo.js';
+import '../packages/checkbox-group-material/lit-checkbox-group-material.js';
 
-import '@lit/item-lumo/lit-item-lumo.js';
-import '@lit/item-material/lit-item-material.js';
+import '../packages/item-lumo/lit-item-lumo.js';
+import '../packages/item-material/lit-item-material.js';
 
-import '@lit/list-box-lumo/lit-list-box-lumo.js';
-import '@lit/list-box-material/lit-list-box-material.js';
+import '../packages/list-box-lumo/lit-list-box-lumo.js';
+import '../packages/list-box-material/lit-list-box-material.js';
 
-import '@lit/progress-bar-lumo/lit-progress-bar-lumo.js';
-import '@lit/progress-bar-material/lit-progress-bar-material.js';
+import '../packages/progress-bar-lumo/lit-progress-bar-lumo.js';
+import '../packages/progress-bar-material/lit-progress-bar-material.js';
 
-import '@lit/radio-button-lumo/lit-radio-button-lumo.js';
-import '@lit/radio-button-material/lit-radio-button-material.js';
+import '../packages/radio-button-lumo/lit-radio-button-lumo.js';
+import '../packages/radio-button-material/lit-radio-button-material.js';
 
-import '@lit/radio-group-lumo/lit-radio-group-lumo.js';
-import '@lit/radio-group-material/lit-radio-group-material.js';
+import '../packages/radio-group-lumo/lit-radio-group-lumo.js';
+import '../packages/radio-group-material/lit-radio-group-material.js';
 
-import '@lit/switch-lumo/lit-switch-lumo.js';
-import '@lit/switch-material/lit-switch-material.js';
+import '../packages/switch-lumo/lit-switch-lumo.js';
+import '../packages/switch-material/lit-switch-material.js';
 
-import '@lit/tab-lumo/lit-tab-lumo.js';
-import '@lit/tab-material/lit-tab-material.js';
+import '../packages/tab-lumo/lit-tab-lumo.js';
+import '../packages/tab-material/lit-tab-material.js';
 
-import '@lit/tabs-lumo/lit-tabs-lumo.js';
-import '@lit/tabs-material/lit-tabs-material.js';
+import '../packages/tabs-lumo/lit-tabs-lumo.js';
+import '../packages/tabs-material/lit-tabs-material.js';
