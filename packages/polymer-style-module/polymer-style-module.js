18f50dc9fb67fbd5b810f00beb08dfdd96546380
web-padawan <serguey.kulikov@gmail.com>
2019-01-28 22:41:39 +0200

    fix: use unsafeCss for constructing style modules

--- packages/polymer-style-module/polymer-style-module.js
+++ packages/polymer-style-module/polymer-style-module.js
@@ -1,6 +1,6 @@
 import { DomModule } from '@polymer/polymer/lib/elements/dom-module.js';
 import { stylesFromTemplate } from '@polymer/polymer/lib/utils/style-gather.js';
-import { CSSResult } from 'lit-element';
+import { unsafeCss } from 'lit-element';
 
 export const getStyleModule = (id, cb) => {
   const template = DomModule.import(id, 'template');
@@ -12,5 +12,5 @@ export const getStyleModule = (id, cb) => {
   if (cb) {
     cssText = cb(cssText);
   }
-  return new CSSResult(cssText);
+  return unsafeCss(cssText);
 };
